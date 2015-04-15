8d14b78eedbf791a3780eff20205a622152331cb
Devin Held <dheld@uci.edu>
2015-04-14 17:57:34 -0700

    Added scratchpad files

---
+++ Scratchpad/AboutViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AboutViewController.swift
+//  Scratchpad
+//
+//  Created by James Linnell on 2/19/15.
+//  Copyright (c) 2015 James Linnell. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @IBOutlet var webView: UIWebView?;
+    
+    
+    
+    override func viewDidLoad() {
+        
+    }
+    
+}
---
+++ Scratchpad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scratchpad
+//
+//  Created by James Linnell on 2/19/15.
+//  Copyright (c) 2015 James Linnell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
